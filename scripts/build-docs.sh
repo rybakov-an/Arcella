496f42e90faec860717cb72162f56062d6557bac
AlexeyRybakov <r262363@yandex.ru>
2025-10-06 03:38:25 +0000

    Update build-docs.sh

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -4,8 +4,4 @@ set -e
 echo "Generating documentation..."
 cargo doc --no-deps
 
-echo "Copying to doc/..."
-rm -rf doc
-cp -r target/doc doc
-
-echo "Done! Documentation available at: ./doc/arcella/index.html"
+echo "Done! Documentation available at: ./target/doc/arcella/index.html"
