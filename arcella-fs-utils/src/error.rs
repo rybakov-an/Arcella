83fc228d18b6d927586ecc5835e9beffb547f69c
Alexey Rybakov <r262363@yandex.ru>
2025-10-22 07:54:26 +0700

    Create new arcella-fs-utols library

---
+++ arcella-fs-utils/src/error.rs
@@ -0,0 +1,38 @@
+// arcella/arcella-wasmtime/src/error.rs
+//
+// Copyright (c) 2025 Arcella Team
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE>
+// or the MIT license <LICENSE-MIT>, at your option.
+// This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use std::path::PathBuf;
+use thiserror::Error;
+
+/// Result type alias for `arcella-wasmtime` operations.
+pub type Result<T> = std::result::Result<T, ArcellaUtilsError>;
+
+/// Errors that can occur during Wasmtime-to-Arcella conversion.
+#[derive(Error, Debug)]
+pub enum ArcellaUtilsError {
+    /// General-purpose error for unexpected conditions.
+    #[error("Internal error: {0}")]
+    Internal(String),
+
+    /// I/O error (file not found, permission denied, etc.).
+    #[error("I/O error: {0}")]
+    Io(#[from] std::io::Error),
+
+    /// IO error with associated path for better diagnostics
+    #[error("I/O error at {path:?}: {source}")]
+    IoWithPath {
+        source: std::io::Error,
+        path: PathBuf,
+    },
+
+    /// TOML error
+    #[error("TOML error: {0}")]
+    TOML(String),
+}
+
